da812e4de87a2250b5fc27e90cc9a13fea0b54e2
YUDAEUN <udu5771@naver.com>
2021-11-08 21:20:10 +0900

    1108

---
+++ C study/LRUD.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+string plans;
+int x=1, y=1;
+
+int dx[4]={0,0,-1,1};
+//L, R, U, D
+int dy[4]={-1,1,0,0};
+char moveTypes[4]={'L','R','U','D'};
+
+int main(void){
+	cin>>n;
+	cin.ignore(); //버퍼 비우기
+	getline(cin,plans); //문자를 받아서 plans에 저장 
+	
+	for(int i=0;i<plans.size();i++){
+		char plan=plans[i];
+		int nx=-1, ny=-1;
+		for(int j=0;j<4;j++){
+			if(plan==moveTypes[j]){
+				nx=x+dx[j];
+				ny=y+dy[j];
+			}
+		}
+		if(nx<1||ny<1||nx>n||ny>n) continue;
+		x=nx;
+		y=ny;
+	} 
+	cout<<x<<' '<<y<<'\n';
+	return 0;
+	
+}
