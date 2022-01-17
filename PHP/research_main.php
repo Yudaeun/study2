9fc7061150e9dceb55416418c5c9b4d2c73d1628
YUDAEUN <udu5771@naver.com>
2022-01-17 23:55:19 +0900

    설문조사 php 틀

---
+++ PHP/research_main.php
@@ -0,0 +1,378 @@
+<form name="applyform" method="POST" action="research_save.php" onSubmit="return customSubmit()">
+
+ <table border=0 width="648" cellpadding=0 cellspacing=10 align=center>
+
+  <tr>
+
+   <td><b>1. 이 카드를 받기 전부터 피셔프라이스 제품을 알고 계셨나요?</b><input type="radio" name="q1" value="Y">예 &nbsp; <input type="radio" name="q1" value="N">아니오</td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td><b>2. 이 카드를 받기 전부터 피셔프라이스에서 바이링구얼(이중언어) 제품이 출시되는걸 알고 계셨나요?</b> <br /> </td>
+
+   
+
+  </tr>
+
+  <tr>
+
+   <td><input type="radio" name="q2" value="Y">예 <input type="radio" name="q2" value="N">아니오</td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+
+
+
+  
+  <tr>
+
+   <td><b>5. 아이의 장난감을 구매하실 때 어떤 점을 가장 중요하게 생각하시나요?</b></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td  style="padding-left:10px">
+
+    <input type="radio" name="q5" value="1">브랜드 선호도<br />
+
+    <input type="radio" name="q5" value="2">제품 인지도<br />
+
+    <input type="radio" name="q5" value="3">가격<br />
+
+    <input type="radio" name="q5" value="4">아이들의 선호도
+
+   </td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+
+
+  <tr>
+
+   <td><b>4. 피셔프라이스 신제품 러닝홈의 가장 만족할만한 점은 무엇인가요?</b></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td  style="padding-left:10px">
+
+    <input type="radio" name="q4" value="1">피셔프라이스 제품이라 믿음이 간다.<br />
+
+    <input type="radio" name="q4" value="2">한글과 영어를 동시에 배울 수 있다는 점이 좋다.<br />
+
+    <input type="radio" name="q4" value="3">보고,듣고,만지면서 아이들의 오감 발달에 도움을 준다.<br />
+
+    <input type="radio" name="q4" value="4">아이가 가장 많이 가지고 노는 장난감이다.
+
+   </td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td><b>5. 아이의 장난감을 구매하실 때 어떤 점을 가장 중요하게 생각하시나요?</b></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td  style="padding-left:10px">
+
+    <input type="radio" name="q5" value="1">브랜드 선호도<br />
+
+    <input type="radio" name="q5" value="2">제품 인지도<br />
+
+    <input type="radio" name="q5" value="3">가격<br />
+
+    <input type="radio" name="q5" value="4">아이들의 선호도
+
+   </td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+  
+  
+  <tr>
+
+   <td><b>5. 아이의 장난감을 구매하실 때 어떤 점을 가장 중요하게 생각하시나요?</b></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td  style="padding-left:10px">
+
+    <input type="radio" name="q5" value="1">브랜드 선호도<br />
+
+    <input type="radio" name="q5" value="2">제품 인지도<br />
+
+    <input type="radio" name="q5" value="3">가격<br />
+
+    <input type="radio" name="q5" value="4">아이들의 선호도
+
+   </td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+  
+  
+  <tr>
+
+   <td><b>5. 아이의 장난감을 구매하실 때 어떤 점을 가장 중요하게 생각하시나요?</b></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td  style="padding-left:10px">
+
+    <input type="radio" name="q5" value="1">브랜드 선호도<br />
+
+    <input type="radio" name="q5" value="2">제품 인지도<br />
+
+    <input type="radio" name="q5" value="3">가격<br />
+
+    <input type="radio" name="q5" value="4">아이들의 선호도
+
+   </td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+  
+  
+  <tr>
+
+   <td><b>5. 아이의 장난감을 구매하실 때 어떤 점을 가장 중요하게 생각하시나요?</b></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td  style="padding-left:10px">
+
+    <input type="radio" name="q5" value="1">브랜드 선호도<br />
+
+    <input type="radio" name="q5" value="2">제품 인지도<br />
+
+    <input type="radio" name="q5" value="3">가격<br />
+
+    <input type="radio" name="q5" value="4">아이들의 선호도
+
+   </td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+  
+  
+  <tr>
+
+   <td><b>5. 아이의 장난감을 구매하실 때 어떤 점을 가장 중요하게 생각하시나요?</b></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td  style="padding-left:10px">
+
+    <input type="radio" name="q5" value="1">브랜드 선호도<br />
+
+    <input type="radio" name="q5" value="2">제품 인지도<br />
+
+    <input type="radio" name="q5" value="3">가격<br />
+
+    <input type="radio" name="q5" value="4">아이들의 선호도
+
+   </td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+  
+  
+  <tr>
+
+   <td><b>5. 아이의 장난감을 구매하실 때 어떤 점을 가장 중요하게 생각하시나요?</b></td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td  style="padding-left:10px">
+
+    <input type="radio" name="q5" value="1">브랜드 선호도<br />
+
+    <input type="radio" name="q5" value="2">제품 인지도<br />
+
+    <input type="radio" name="q5" value="3">가격<br />
+
+    <input type="radio" name="q5" value="4">아이들의 선호도
+
+   </td>
+
+  </tr>
+
+
+
+  <tr>
+
+   <td height="4"></td>
+
+  </tr>
+  
+
+
+
+
+
+
+  <tr>
+
+   <td><b>8. 귀하께서는 피셔프라이스 신제품 래프앤런에 대해 주위사람들에게 어떤 이야기를 들려주고 싶으신가요?</b></td>
+
+  </tr>
+
+  <tr>
+
+   <td  style="padding-left:10px"><textarea name="q8" style="width:600px; height:60px; border:1px solid #D6D6D6; overflow:hidden; text-overflow:ellipsis;"></textarea></td>
+
+  </tr>
+
+</table>
+
+function customSubmit()
+{
+
+ var f = document.applyform ;
+
+
+
+ if( f.q1[0].checked == false && f.q1[1].checked == false) {
+  alert("1번 문제 답변을 선택해 주세요.") ; 
+
+  f.q1[0].focus() ; 
+
+  return false ;
+
+}
+return true;
+}
+
+
+<?
+ $con=mysqli_connect("localhost","root","21912165", "reseachdb") or die("MYSQL 접속 실패 !!");
+
+ 
+ $email = $_REQUEST[email1]."@".$_REQUEST[email3] ; //두개로 입력된 값을 하나로 묶어 디비에 저장합니다. 아래 phone, zipcode도 마찮가지 입니다.
+ $phone = $_REQUEST[phone1]."-".$_REQUEST[phone2]."-".$_REQUEST[phone3] ;
+ $zipcode = $_REQUEST[zipcode1]."-".$_REQUEST[zipcode2] ;
+
+
+ $query = "insert into brandcard( brand_id,q1,q2,q3,q4,q5,q6,q7,q8,name,serial,gender,age,baby,email,phone,zipcode,addr,is_email,is_sms,regdate)  " ;
+ $query.= "values( 'fisherprice','$q1','$q2','$q3','$q4','$q5','$q6','$q7','$q8','$name','$serial','$gender','$age','$baby','$email','$phone','$zipcode','$addr','$is_email','$is_sms',now())  " ;
+
+
+ $result = mysql_query($query) ;
+
+ if(mysql_error()) {
+        echo $query."<br>" ;
+        mysql_close($conn) ;
+        exit ;
+    }
+
+
+ if($conn) mysql_close($conn) ;
+?>
+
+
+<script>
+alert("참여해 주셔서 감사합니다.") ;
+location = "history.back(-1)" ;
+</script>
