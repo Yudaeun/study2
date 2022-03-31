f19849092c7367b0dad2cb497ed7a565bab40c17
YUDAEUN <udu5771@naver.com>
2022-03-31 23:34:10 +0900

    study

---
+++ R/homework3.R
@@ -0,0 +1,13 @@
+install.packages("ggplot2")
+install.packages("dplyr")
+library(ggplot2)
+library(dplyr)
+midwest_raw<-as.data.frame(ggplot2::midwest)
+midwest_new<-midwest_raw
+midwest_new<-midwest_new %>% mutate(adultper=(popadults/poptotal)*100)
+midwest_new %>% arrange(desc(adultper)) %>%
+  select(county,adultper) %>%
+  head(5)
+midwest_new<-midwest_new %>%
+  mutate(adultgrade=ifelse(adultper>=70,"large",
+                           ifelse(adultper>=60,"middle","small")))
