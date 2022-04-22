6eae3066b8ae2b2037eb641017fb5a8178d58203
YUDAEUN <udu5771@naver.com>
2022-04-22 21:57:25 +0900

    sequential explore

---
+++ Python/ch01/29.py
@@ -0,0 +1,14 @@
+# 순차탐색
+def sequential_search(n,target,array):
+    for i in range(n):
+        if array[i]==target:
+            return i+1
+
+print("생성할 원소 개수를 입력하고 찾을 문자열을 입력하세요")
+input_data=input().split()
+n=int(input_data[0])
+target=input_data[1]
+
+print("적은 원소 개수만큼의 문자열을 입력하세요")
+array=input().split()
+print(sequential_search(n,target,array))
