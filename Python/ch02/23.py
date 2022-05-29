36b9d13cc33c524fb028471e65d96138193c5f06
YUDAEUN <udu5771@naver.com>
2022-05-29 19:53:42 +0900

    python

---
+++ Python/ch02/23.py
@@ -0,0 +1,9 @@
+# 완주하지 못한 선수
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+
+    for i, j in zip(participant, completion):
+        if i != j:
+            return i
+    return participant[-1]
