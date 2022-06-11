0b35da64556255b20925d6084cb0371d40914196
YUDAEUN <udu5771@naver.com>
2022-06-11 21:06:18 +0900

    알고리즘

--- Python/ch03/11.py
+++ Python/ch03/11.py
@@ -1,16 +1,25 @@
 # 큰 수 만들기
+from functools import cmp_to_key
 
-n,m=map(int,input().split())
-temp=0
-mod=0
-save=0
-answer=[]
+n=int(input())
+m=list(map(int,input().split()))
+
+def compare(x,y):
+    if str(x)+str(y)>str(y)+str(x):
+        return -1 #x가 먼저 정렬된다.
+    elif str(x)+str(y)<str(y)+str(x):
+        return 1 # y가 먼저 정렬된다.
+    return 0 # 순서가 변하지 않음
+
+m.sort(key=cmp_to_key(compare)) # cmp_to_key는 두 수의 str화 되어 있는 숫자일 때 비교해서 정렬한다.
+count=0
 
 for i in m:
-    if (i%10)>(save%10):
-        answer.insert(0,i)
-        save=i
-    if (i%10)==0:
-        answer.append(i)
-    print(answer)
-print(answer)
+    if i==0:
+        count+=1 #0의 개수를 센다.
+if count==len(m): # 만약 전부 0이라면 0 하나만 출력
+    print(0)
+else:
+    for i in m:
+        print(i,end='')
+
