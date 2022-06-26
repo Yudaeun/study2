c448a16d599e1bbed255cf588887eb615fd9b487
YUDAEUN <udu5771@naver.com>
2022-06-26 23:17:48 +0900

    크루스칼

---
+++ Python/ch04/04.py
@@ -0,0 +1,40 @@
+# 도시 분할 계획
+def find_parent(parent,x):
+    #if not root, call func
+    if parent[x]!=x:
+        parent[x]=find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a=find_parent(parent,a)
+    b=find_parent(parent,b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+#노드 개수, 간선 개수
+v,e=map(int,input().split())
+parent=[0]*(v+1)
+
+edges=[]
+result=0
+
+for i in range(1,v+1):
+    parent[i]=i
+
+for _ in range(e):
+    a,b,cost=map(int,input().split())
+    edges.append((cost,a,b))
+    # first tuple elment to sort by cost
+
+edges.sort()
+last=0
+
+for edge in edges:
+    cost,a,b=edge
+    if find_parent(parent,a)!=find_parent(parent,b):
+        union_parent(parent,a,b)
+        result+=cost
+        last=cost
+print(result-last)
