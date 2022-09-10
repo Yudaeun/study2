59e4a9e3e400fd1c923d49fd82d024701b8cbcaa
YUDAEUN <udu5771@naver.com>
2022-09-10 15:41:26 +0900

    영어 끝말잇기

--- Python/ch06/30.py
+++ Python/ch06/30.py
@@ -1,17 +1,21 @@
-#Fly me to the Alpha Centauri
+import math
+#영어 끝말잇기
+def solution(n, words):
+    answer = []
+    temp = [words[0]]
+    for i in range(1, len(words)):
+        last = words[i - 1][-1]  # 이전 단어의 마지막 문자 저장
+        if (last != words[i][0]) or (words[i] in temp):
+            mod = (i + 1) % n
+            if mod == 0:
+                answer.append(n)
+            else:
+                answer.append(mod)
 
-t=int(input())
+            answer.append(math.ceil((i + 1) / n))
+            return answer
 
-for _ in range(t):
-    x,y=map(int,input().split())
+        temp.append(words[i])  # 스택에 현재 단어 넣기
 
-    d=y-x
-    n=0
-    while True:
-        if d<n*(n+1):
-            break
-        n+=1
-    if d<=n**2:
-        print(n*2-1)
-    else:
-        print(n*2)
+    zero = [0, 0]
+    return zero
